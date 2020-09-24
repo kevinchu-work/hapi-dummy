341afb2b11282c002b8ea50ad16ceb0757ded90c
Kevin <kevinchu.work@gmail.com>
2020-09-24 10:00:02 +0800

    - Accepting POST & GET request - Joi validation setting disabled (because of GET request)

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
 const Hapi = require('@hapi/hapi');
+// const Joi = require('@hapi/joi');
 
 const init = async () => {
 
+  const HapiListeningPort = 3000,
+        HapiListeningAddr = '0.0.0.0';
+
   const server = Hapi.server({
-    port: 3000,
-    host: '0.0.0.0'
+    port: HapiListeningPort,
+    host: HapiListeningAddr
   });
 
   // --------------------------------
   // route --------------------------
   // --------------------------------
-  server.route({
-    method: 'GET',
+  let routes = [];
+  
+  // Search
+  routes.push({
     path: '/search',
+    method: ['POST', 'GET'],
     handler: (req, h) => {
-      return 'Search Result';
-    }
+
+      let params = (req.method === "get") ? req.query : req.payload;
+      console.log(`Search by ${req.method}: ${params.q}`);
+
+      return { keyword: params.q }
+
+    },
+    // Validation - Joi
+    // for POST method only
+    //
+    // options: {
+    //   auth: false,
+    //   validate: {
+    //     payload: Joi.object({
+    //       q: Joi.string().min(3).max(8)
+    //     })
+    //   }
+    // }
   })
 
+
+  // --------------------------------
+  // Wrap it up -----------------------
+  // --------------------------------
+  server.route(routes);
+
   await server.start();
-  console.log('Server running on port 3000');
+  console.log(`Server running on port ${HapiListeningPort}`);
 };
 
 init();
